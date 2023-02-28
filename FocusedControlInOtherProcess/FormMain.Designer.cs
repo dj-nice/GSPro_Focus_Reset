431fbab7922e894529deb59a8c3869f2b6ce119a
dj-nice <info@dj-nice.net>
2023-02-28 19:48:31 +0100

    Add files via upload

--- FocusedControlInOtherProcess/FormMain.Designer.cs
+++ FocusedControlInOtherProcess/FormMain.Designer.cs
@@ -28,9 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.labelHeader = new System.Windows.Forms.Label();
-            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
             this.buttonStart = new System.Windows.Forms.Button();
             this.buttonStop = new System.Windows.Forms.Button();
             this.tBInterval = new System.Windows.Forms.TextBox();
@@ -39,8 +37,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.labelAppName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.labelAppName = new System.Windows.Forms.Label();
+            this.bg2 = new System.ComponentModel.BackgroundWorker();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,28 +50,40 @@
             this.labelHeader.Location = new System.Drawing.Point(12, 29);
             this.labelHeader.Name = "labelHeader";
             this.labelHeader.Size = new System.Drawing.Size(205, 20);
-            this.labelHeader.TabIndex = 0;
-            this.labelHeader.Text = "app to monitor:";
+            this.labelHeader.TabIndex = 10;
+            this.labelHeader.Text = "set foreground app:";
             this.labelHeader.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(105, 111);
+            this.buttonStart.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonStart.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buttonStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.ForeColor = System.Drawing.Color.DarkGreen;
+            this.buttonStart.Location = new System.Drawing.Point(104, 111);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
-            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Size = new System.Drawing.Size(90, 23);
+            this.buttonStart.TabIndex = 2;
             this.buttonStart.Text = "start";
-            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.UseVisualStyleBackColor = false;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // buttonStop
             // 
-            this.buttonStop.Location = new System.Drawing.Point(19, 111);
+            this.buttonStop.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonStop.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.buttonStop.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonStop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStop.ForeColor = System.Drawing.Color.Red;
+            this.buttonStop.Location = new System.Drawing.Point(8, 111);
             this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.Size = new System.Drawing.Size(90, 23);
             this.buttonStop.TabIndex = 2;
             this.buttonStop.Text = "stop";
-            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.UseVisualStyleBackColor = false;
             this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
             // 
             // tBInterval
@@ -81,6 +93,8 @@
             this.tBInterval.Size = new System.Drawing.Size(71, 20);
             this.tBInterval.TabIndex = 3;
             this.tBInterval.Text = "3000";
+            this.tBInterval.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tBInterval_KeyDown);
+            this.tBInterval.Leave += new System.EventHandler(this.tBInterval_Leave);
             // 
             // label1
             // 
@@ -93,11 +107,14 @@
             // 
             // tBAppName
             // 
+            this.tBAppName.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tBAppName.Location = new System.Drawing.Point(8, 98);
             this.tBAppName.Name = "tBAppName";
             this.tBAppName.Size = new System.Drawing.Size(163, 20);
             this.tBAppName.TabIndex = 5;
             this.tBAppName.Text = "GSPro.exe";
+            this.tBAppName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tBAppName_KeyDown);
+            this.tBAppName.Leave += new System.EventHandler(this.tBAppName_Leave);
             // 
             // label2
             // 
@@ -110,16 +127,17 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(190, 111);
+            this.button3.Location = new System.Drawing.Point(198, 111);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(27, 23);
-            this.button3.TabIndex = 7;
+            this.button3.TabIndex = 1;
             this.button3.Text = ">>";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.buttonZoom_Click);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.tBInterval);
@@ -131,16 +149,15 @@
             this.panel1.TabIndex = 8;
             this.panel1.Visible = false;
             // 
-            // labelAppName
+            // button1
             // 
-            this.labelAppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAppName.ForeColor = System.Drawing.Color.Red;
-            this.labelAppName.Location = new System.Drawing.Point(15, 61);
-            this.labelAppName.Name = "labelAppName";
-            this.labelAppName.Size = new System.Drawing.Size(205, 20);
-            this.labelAppName.TabIndex = 9;
-            this.labelAppName.Text = "-";
-            this.labelAppName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(6, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "default Values";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
@@ -152,12 +169,28 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "(1000 - 10000 ms)";
             // 
+            // labelAppName
+            // 
+            this.labelAppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAppName.ForeColor = System.Drawing.Color.Red;
+            this.labelAppName.Location = new System.Drawing.Point(12, 61);
+            this.labelAppName.Name = "labelAppName";
+            this.labelAppName.Size = new System.Drawing.Size(205, 20);
+            this.labelAppName.TabIndex = 9;
+            this.labelAppName.Text = "-";
+            this.labelAppName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // bg2
+            // 
+            this.bg2.WorkerSupportsCancellation = true;
+            this.bg2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bg2_DoWork);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(219, 142);
+            this.ClientSize = new System.Drawing.Size(230, 142);
             this.Controls.Add(this.labelAppName);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button3);
@@ -168,10 +201,11 @@
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(445, 181);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(235, 181);
+            this.MinimumSize = new System.Drawing.Size(246, 181);
             this.Name = "FormMain";
+            this.ShowIcon = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
-            this.Text = "Force GSPro AppFocus";
+            this.Text = "Force & Reset AppFocus";
             this.TopMost = true;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -193,6 +227,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label labelAppName;
         private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bg2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
