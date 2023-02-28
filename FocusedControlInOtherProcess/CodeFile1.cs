afad500780dd3fccb95c0523928d9b217e599e2b
dj-nice <info@dj-nice.net>
2023-02-28 19:50:13 +0100

    updateDelete FocusedControlInOtherProcess directory

--- FocusedControlInOtherProcess/CodeFile1.cs
+++
@@ -1,8 +0,0 @@
-﻿// [DllImport("user32.dll")]
-// static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
-
-//[DllImport("user32.dll")]
-//static extern IntPtr AttachThreadInput(IntPtr idAttach, IntPtr idAttachTo, bool fAttach);
-
-//[DllImport("user32.dll")]
-//static extern IntPtr GetFocus();
