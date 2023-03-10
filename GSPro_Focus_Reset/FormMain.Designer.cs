8a12adfec2848ad7004b98b5ac7f3970416cf273
alex <alex@DESKTOP-JR-2022>
2023-03-10 23:57:27 +0100

    100323

---
+++ GSPro_Focus_Reset/FormMain.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace FocusedControlInOtherProcess
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.tBInterval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tBAppName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rB_Focus = new System.Windows.Forms.RadioButton();
+            this.rB_onTop = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelAppName = new System.Windows.Forms.Label();
+            this.bg2 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.Location = new System.Drawing.Point(12, 29);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(205, 20);
+            this.labelHeader.TabIndex = 10;
+            this.labelHeader.Text = "set foreground app:";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonStart.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buttonStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.ForeColor = System.Drawing.Color.DarkGreen;
+            this.buttonStart.Location = new System.Drawing.Point(104, 111);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(90, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonStop.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.buttonStop.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonStop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStop.ForeColor = System.Drawing.Color.Red;
+            this.buttonStop.Location = new System.Drawing.Point(8, 111);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(90, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "stop";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // tBInterval
+            // 
+            this.tBInterval.Location = new System.Drawing.Point(6, 55);
+            this.tBInterval.Name = "tBInterval";
+            this.tBInterval.Size = new System.Drawing.Size(71, 20);
+            this.tBInterval.TabIndex = 3;
+            this.tBInterval.Text = "3000";
+            this.tBInterval.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tBInterval_KeyDown);
+            this.tBInterval.Leave += new System.EventHandler(this.tBInterval_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Interval in ms:";
+            // 
+            // tBAppName
+            // 
+            this.tBAppName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tBAppName.Location = new System.Drawing.Point(8, 98);
+            this.tBAppName.Name = "tBAppName";
+            this.tBAppName.Size = new System.Drawing.Size(163, 20);
+            this.tBAppName.TabIndex = 5;
+            this.tBAppName.Text = "GSPro.exe";
+            this.tBAppName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tBAppName_KeyDown);
+            this.tBAppName.Leave += new System.EventHandler(this.tBAppName_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "*.exe name of app to focus";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(198, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonZoom_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rB_Focus);
+            this.panel1.Controls.Add(this.rB_onTop);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tBInterval);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tBAppName);
+            this.panel1.Location = new System.Drawing.Point(243, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(197, 175);
+            this.panel1.TabIndex = 8;
+            this.panel1.Visible = false;
+            // 
+            // rB_Focus
+            // 
+            this.rB_Focus.AutoSize = true;
+            this.rB_Focus.Location = new System.Drawing.Point(8, 148);
+            this.rB_Focus.Name = "rB_Focus";
+            this.rB_Focus.Size = new System.Drawing.Size(185, 17);
+            this.rB_Focus.TabIndex = 10;
+            this.rB_Focus.TabStop = true;
+            this.rB_Focus.Text = "Force App stay on top + get focus";
+            this.rB_Focus.UseVisualStyleBackColor = true;
+            this.rB_Focus.CheckedChanged += new System.EventHandler(this.rB_Focus_CheckedChanged);
+            // 
+            // rB_onTop
+            // 
+            this.rB_onTop.AutoSize = true;
+            this.rB_onTop.Checked = true;
+            this.rB_onTop.Location = new System.Drawing.Point(8, 125);
+            this.rB_onTop.Name = "rB_onTop";
+            this.rB_onTop.Size = new System.Drawing.Size(129, 17);
+            this.rB_onTop.TabIndex = 9;
+            this.rB_onTop.TabStop = true;
+            this.rB_onTop.Text = "Force App stay on top";
+            this.rB_onTop.UseVisualStyleBackColor = true;
+            this.rB_onTop.CheckedChanged += new System.EventHandler(this.rB_onTop_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "default Values";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "(1000 - 10000 ms)";
+            // 
+            // labelAppName
+            // 
+            this.labelAppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAppName.ForeColor = System.Drawing.Color.Red;
+            this.labelAppName.Location = new System.Drawing.Point(12, 61);
+            this.labelAppName.Name = "labelAppName";
+            this.labelAppName.Size = new System.Drawing.Size(205, 20);
+            this.labelAppName.TabIndex = 9;
+            this.labelAppName.Text = "-";
+            this.labelAppName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // bg2
+            // 
+            this.bg2.WorkerSupportsCancellation = true;
+            this.bg2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bg2_DoWork);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(230, 146);
+            this.Controls.Add(this.labelAppName);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(460, 220);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(246, 185);
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Force & Reset AppFocus";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeader;
+       
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox tBInterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBAppName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelAppName;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bg2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rB_Focus;
+        private System.Windows.Forms.RadioButton rB_onTop;
+    }
+}
+
---
+++ GSPro_Focus_Reset/FormMain.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FocusedControlInOtherProcess
+{
+    public partial class FormMain : Form
+    {
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        private static string AppName;
+        int interval = 3000;
+        string interval2 = "";
+        int dfinterval = 3000;
+        string dfAppName = "GSPro.exe";
+
+        public FormMain()
+        {
+            InitializeComponent();
+            FillTextBox();
+            interval = int.TryParse(tBInterval.Text, out interval) ? interval : dfinterval;
+            Start();
+        }
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        //static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        //static readonly IntPtr HWND_TOP = new IntPtr(0);
+        //static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+      //const UInt32 SWP_NOZORDER = 0x0004;
+      //const UInt32 SWP_NOREDRAW = 0x0008;
+        const UInt32 SWP_NOACTIVATE = 0x00010;
+      //const UInt32 SWP_FRAMECHANGED = 0x00020;
+        const UInt32 SWP_SHOWWINDOW = 0x00040;
+      //const UInt32 SWP_HIDEWINDOW = 0x00080;
+      //const UInt32 SWP_NOCOPYBITS = 0x000100;
+      //const UInt32 SWP_NOOWNERZORDER = 0x000200;
+      //const UInt32 SWP_NOSENDCHANGING = 0x000400;
+      //const UInt32 SWP_DEFERERASE = 0x0002000;
+      //const UInt32 SWP_ASYNCWINDOWPOS = 0x0004000;
+
+    //SWP_NOSIZE verhindert, dass das Fenster eine neue Größe bekommt.cx und cy sind dann irrelevant und können auf 0 gesetzt werden.
+    //SWP_NOMOVE verhindert, dass das Fenster verschoben wird.x und y sind dann irrelevant und können auf 0 gesetzt werden.
+    //SWP_NOZORDER verhindert, dass die Z-Order-Position verändert wird.
+    //SWP_NOREDRAW verhindert, dass irgendetwas automatisch neu gezeichnet wird. Das betrifft sowohl das Fenster selbst, aber auch alle verdeckten Fenster werden nicht invalidiert.
+    //SWP_NOACTIVATE verhindert, dass das Fenster den Fokus erhält.
+    //SWP_FRAMECHANGED wird benutzt um Änderungen der SetWindowLong-Funktion anzuwenden. Sendet eine WM_NCCALCSIZE-Nachricht an das Fenster.
+    //SWP_SHOWWINDOW sorgt dafür, dass das Fenster sichtbar wird.Entspricht.Show() bzw. den Ändern der Visible-Eigenschaft.
+    //SWP_HIDEWINDOW sorgt dafür, dass das Fenster unsichtbar wird.Entspricht.Hide() bzw. den Ändern der Visible-Eigenschaft.
+    //SWP_NOCOPYBITS verwirft den kompletten dargestellten Fensterinhalt und sorgt so für ein vollständiges Neuzeichnen.
+    //SWP_NOOWNERZORDER verschiebt nicht das besitzende Fenster in der Z-Order.
+    //SWP_NOSENDCHANGING verhindert, dass das Fenster die WM_WINDOWPOSCHANGING-Nachricht erhält
+    //SWP_DEFERERASE verhindert, dass das Fenster die WM_SYNCPAINT-Nachricht erhält
+    //SWP_ASYNCWINDOWPOS verhindert das der aufrufende Thread durch den Thread, der das Fenster verarbeitet, blockiert werden kann.
+
+
+    [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+        // Old Way < V14
+        // [DllImport("user32.dll")]
+        //  public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+        
+        //[DllImport("User32.dll")]
+        //private static extern int ShowWindow(int hwnd, int nCmdShow);
+        
+        // Win32 API Constants for ShowWindowAsync()
+        //private const int SW_HIDE = 0;
+        //private const int SW_SHOWNORMAL = 1;
+        //private const int SW_NORMAL = 1;
+        //private const int SW_SHOWMINIMIZED = 2;
+        //private const int SW_SHOWMAXIMIZED = 3;
+        //private const int SW_MAXIMIZE = 3;
+        //private const int SW_SHOWNOACTIVATE = 4;
+        //private const int SW_SHOW = 5;
+        //private const int SW_MINIMIZE = 6;
+        //private const int SW_SHOWMINNOACTIVE = 7;
+        //private const int SW_SHOWNA = 8;
+          private const int SW_RESTORE = 9;
+        //private const int SW_SHOWDEFAULT = 10;
+        //private const int SW_FORCEMINIMIZE = 11;
+        //private const int SW_MAX = 11;
+
+        private void Start()
+        {
+            AppName = tBAppName.Text;
+            int fileExtPos = AppName.LastIndexOf(".");
+            if (fileExtPos >= 0)
+                AppName = AppName.Substring(0, fileExtPos);
+            if (tBAppName.Text != "")
+            {
+                labelAppName.ForeColor = Color.Red;
+                labelAppName.Text = AppName;
+            }
+            try
+            {
+                bg2.RunWorkerAsync();
+                buttonStart.Enabled = false;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+        private void bg2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            while (!bg2.CancellationPending)
+            {
+                try
+                {
+                    FocusProcess(AppName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:\n\n" + ex.Message, "System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                Thread.Sleep(interval);
+            }
+            e.Cancel = true;
+        }
+        private void FocusProcess(string fileName)
+        {
+            AppName = fileName;
+            IntPtr hWnd;
+            Process[] processRunning = Process.GetProcesses();
+            foreach (Process pr in processRunning)
+            {
+                if (pr.ProcessName.ToLower() == AppName.ToLower())
+                {
+                    hWnd = pr.MainWindowHandle; //use it as IntPtr not int
+
+                    if (rB_onTop.Checked)
+                    {
+                        //V14:
+                        // Old Way 1st: ShowWindowAsync(new HandleRef(null, hWnd), SW_RESTORE);
+                        // Old Way 2nd: SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+                        //New Way: 
+                        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOACTIVATE + SWP_SHOWWINDOW + SWP_NOSIZE + SWP_NOMOVE);
+                    }
+                        if(rB_Focus.Checked) 
+                    {
+                        SetForegroundWindow(hWnd);
+                        //V14:
+                        // Old Way 1st: ShowWindowAsync(new HandleRef(null, hWnd), SW_RESTORE);
+                        // Old Way 2nd: ActivateWindow(hWnd);
+                        // New Way:
+                        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_SHOWWINDOW + SWP_NOSIZE + SWP_NOMOVE);
+                    }
+                    labelAppName.BeginInvoke(new Action(() => { labelAppName.ForeColor = System.Drawing.Color.Green; ; }));
+                    
+                }
+            }
+        }
+
+        //Solution found: https://stackoverflow.com/questions/10740346/setforegroundwindow-only-working-while-visual-studio-is-open
+        private const int ALT = 0xA4;
+        private const int EXTENDEDKEY = 0x1;
+        private const int KEYUP = 0x2;
+        private const uint Restore = 9;
+
+        // Old Way < V14
+        //[DllImport("user32.dll")]
+        //private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+        // Old Way < V14
+        //[DllImport("user32.dll")]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //private static extern bool IsIconic(IntPtr hWnd);
+        // Old Way < V14
+        //[DllImport("user32.dll")]
+        //private static extern int ShowWindow(IntPtr hWnd, uint Msg);
+        // Old Way < V14
+        //[DllImport("user32.dll")]
+        //private static extern IntPtr GetForegroundWindow();
+        
+        // Old Way < V14
+        //public static void ActivateWindow(IntPtr mainWindowHandle)
+        //{
+        //    //check if already has focus
+        //    if (mainWindowHandle == GetForegroundWindow()) return;
+        //    //check if window is minimized
+        //    if (IsIconic(mainWindowHandle))
+        //    {
+        //        ShowWindow(mainWindowHandle, Restore);
+            
+        //    }
+        //    // Simulate a key press
+        //    keybd_event(0, 0, 0, 0);
+        //    SetForegroundWindow(mainWindowHandle);
+        //}
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            setConfig();
+            Start();
+            buttonStop.Focus();
+        }
+
+        private async void buttonStop_Click(object sender, EventArgs e)
+        {
+            bg2.CancelAsync();
+            bg2.Dispose();
+            labelAppName.ForeColor = Color.Red;
+            buttonStart.Text = "Wait " + (interval / 1000).ToString() + " sec.";
+            await Task.Delay(interval);
+            buttonStart.Text = "Start";
+            buttonStart.Enabled = true;
+            buttonStart.Focus();
+        }
+
+        private void buttonZoom_Click(object sender, EventArgs e)
+        {
+            if (panel1.Visible == true) infozoom(true);
+            else infozoom(false);
+        }
+
+        private void infozoom(bool zoom)
+        {
+            if (zoom == false)
+            {
+                panel1.Visible = true;
+                this.Width = 460;
+                this.Height = 220;
+                button3.Text = "<<";
+            }
+            else
+            {
+                panel1.Visible = false;
+                this.Width = 246;
+                this.Height = 185;
+                button3.Text = ">>";
+            }
+        }
+        private void setConfig()
+        {
+            //make changes
+            config.AppSettings.Settings["AppName"].Value = tBAppName.Text;
+            config.AppSettings.Settings["Interval"].Value = tBInterval.Text;
+            if (rB_Focus.Checked)
+            {
+                config.AppSettings.Settings["forceFocus"].Value = "true";
+            }
+            else 
+            { 
+                config.AppSettings.Settings["forceFocus"].Value = "false";
+            }
+            //save to apply changes
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+        private void FillTextBox()
+        {
+            if (ConfigurationManager.AppSettings["AppName"] != null)
+            {
+                AppName = ConfigurationManager.AppSettings["AppName"];
+                tBAppName.Text = AppName;
+            }
+            else
+            {
+                tBAppName.Text = dfAppName;
+            }
+            if (ConfigurationManager.AppSettings["interval"] != null)
+            {
+                interval2 = ConfigurationManager.AppSettings["interval"];
+                tBInterval.Text = interval2;
+            }
+            else
+            {
+                tBInterval.Text = dfinterval.ToString();
+            }
+            if (ConfigurationManager.AppSettings["forceFocus"] == "true")
+            {
+                rB_Focus.Checked= true;
+            }
+            else
+            {
+                rB_onTop.Checked = true;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tBInterval.Text = dfinterval.ToString();
+            tBAppName.Text = dfAppName;
+            tBAppName.Update();
+            tBInterval.Update();
+            setConfig();
+        }
+
+        private void tBInterval_Leave(object sender, EventArgs e)
+        {
+            if (!int.TryParse(tBInterval.Text, out _))
+            {
+                MessageBox.Show("Only numbers please!" + Environment.NewLine + "set default interval = 3000");
+                tBInterval.Text = dfinterval.ToString();
+            }
+            setConfig();
+            interval2 = tBInterval.Text;
+            interval = int.TryParse(tBInterval.Text, out interval) ? interval : dfinterval;
+        }
+
+        private void tBAppName_Leave(object sender, EventArgs e)
+        {
+            setConfig();
+        }
+
+        private void tBInterval_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                tBAppName.Focus();
+            }
+        }
+
+        private void tBAppName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                tBInterval.Focus();
+            }
+        }
+
+        private void rB_Focus_CheckedChanged(object sender, EventArgs e)
+        {
+            setConfig();
+        }
+
+        private void rB_onTop_CheckedChanged(object sender, EventArgs e)
+        {
+            setConfig();
+        }
+    }
+}
